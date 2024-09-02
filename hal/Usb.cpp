76887233c5b9e898f6ad0c1ed1a9b6ebcb44cef9
Udipto Goswami <quic_ugoswami@quicinc.com>
2024-09-02 12:23:43 +0530

    USB: HAL: Fix PortRole mode type usecases for assigning role correctly
    Commit e51d302dc9a4 (“USB: Various minor style cleanups”) reworked the
    getCurrentRoleHelper function to use the appendRoleNodeHelper API for
    setting the correct role. However, this change inadvertently caused the
    function to read port_type instead of data_role. This misalignment led
    to confusion in the HAL, as the values of source/sink for port_type
    resemble those of power_role.
    
    To resolve this issue, revert to the previous method of directly using
    the filename and ensure data_role is read when port_type is present.
    Also, removing the dual check since this won't be used anymore.
    
    Change-Id: Idadd639a22caa5bb0b49b9b8edfb60725fa064f3
    (cherry picked from commit 2586dd4b9239fcb146bfb840c2f311a746ed1763)

--- hal/Usb.cpp
+++ hal/Usb.cpp
@@ -174,17 +174,12 @@ static std::string appendRoleNodeHelper(const std::string &portName, PortRole::T
     std::string node("/sys/class/typec/" + portName);
 
     switch (tag) {
-      case PortRole::mode: {
-        std::string port_type(node + "/port_type");
-        if (!access(port_type.c_str(), F_OK))
-          return port_type;
-        // port_type doesn't exist for UCSI. in that case fall back to data_role
-      }
-      //fall-through
       case PortRole::dataRole:
         return node + "/data_role";
       case PortRole::powerRole:
         return node + "/power_role";
+      case PortRole::mode:
+        return node + "/port_type";
       default:
         return "";
     }
@@ -343,10 +338,13 @@ static Status getCurrentRoleHelper(const std::string &portName, bool connected,
   // Mode
 
   if (currentRole.getTag() == PortRole::powerRole) {
+    filename = "/sys/class/typec/" + portName + "/power_role";
     currentRole.set<PortRole::powerRole>(PortPowerRole::NONE);
   } else if (currentRole.getTag() == PortRole::dataRole) {
+    filename = "/sys/class/typec/" + portName + "/data_role";
     currentRole.set<PortRole::dataRole>(PortDataRole::NONE);
   } else if (currentRole.getTag() == PortRole::mode) {
+    filename = "/sys/class/typec/" + portName + "/data_role";
     currentRole.set<PortRole::mode>(PortMode::NONE);
   } else {
     return Status::ERROR;
@@ -368,7 +366,6 @@ static Status getCurrentRoleHelper(const std::string &portName, bool connected,
     }
   }
 
-  filename = appendRoleNodeHelper(portName, currentRole.getTag());
   if (!ReadFileToString(filename, &roleName)) {
     ALOGE("getCurrentRole: Failed to open filesystem node: %s", filename.c_str());
     return Status::ERROR;
@@ -390,8 +387,6 @@ static Status getCurrentRoleHelper(const std::string &portName, bool connected,
       currentRole.set<PortRole::dataRole>(PortDataRole::DEVICE);
     else
       currentRole.set<PortRole::mode>(PortMode::UFP);
-  } else if (roleName == "dual") {
-     currentRole.set<PortRole::mode>(PortMode::DRP);
   } else if (roleName != "none") {
     /* case for none has already been addressed.
      * so we check if the role isn't none.
